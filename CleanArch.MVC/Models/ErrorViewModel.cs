f767f86121d5d7c032266190dc75a15d73a2e9ae
Felipe <luisfelipe7014feups@gmail.com>
2025-07-11 11:29:03 -0300

    Criando Diretorios e Projetos. E adicionando referencias

---
+++ CleanArch.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CleanArch.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
